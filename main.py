f4532b331a8b975f2f000ffe76a62327119c64aa
Kyu Lee <kwooler@gmail.com>
2025-10-19 17:23:54 -0700

    A75 init

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
-
 def isSubset(num1, num2):
     """
     ########################################
     Code Your Program here
     ########################################
     """
+    for i in range(len(num2) - len(num1) + 1):
+        if num1 == num2[i:i+len(num1)]:
+            return True
+    return False
 
 
 def main():
     num1 = [20, 30, 35]
     num2 = [5, 20, 30, 35, 50]
+    print(f'\n****************** Test Case 1 ******************')
+    print(f'num1: {num1}, num2: {num2}')
     ret = isSubset(num1, num2)
-    print(f'Your return value is {ret}')  # True
+    print(f'Your return value is {ret}, expected True')  # True
 
     num1 = [1, 3, 2]
     num2 = [1, 2, 3, 4, 5]
+    print(f'\n****************** Test Case 2 ******************')
+    print(f'num1: {num1}, num2: {num2}')
     ret = isSubset(num1, num2)
-    print(f'Your return value is {ret}')  # False
+    print(f'Your return value is {ret}, expected False')  # False
 
     num1 = [1, 4, 5]
     num2 = [1, 2, 3, 4, 5]
+    print(f'\n****************** Test Case 3 ******************')
+    print(f'num1: {num1}, num2: {num2}')
     ret = isSubset(num1, num2)
-    print(f'Your return value is {ret}')  # False
+    print(f'Your return value is {ret}, expected False')  # False
 
     num1 = [1, 3, 2]
     num2 = [4, 1, 3, 2, 5]
+    print(f'\n****************** Test Case 4 ******************')
+    print(f'num1: {num1}, num2: {num2}')
     ret = isSubset(num1, num2)
-    print(f'Your return value is {ret}')  # True
+    print(f'Your return value is {ret}, expected True')  # True
 
 
 if __name__ == '__main__':
--- main_test.py
+++ main_test.py
@@ -1,47 +1,42 @@
 import main
-import io
-import sys
-import re
-import math
+import pytest
 
-
+@pytest.mark.basic
 def test_main_1():
-    captureout = io.StringIO()
-    sys.stdout = captureout
-    # datastr = '1 3 2\n1 2 3 4 5'
-    # sys.stdin = io.StringIO(datastr)
-
-    sys.stdout = sys.__stdout__
-    print('captured ', captureout.getvalue())
-    lines = captureout.getvalue().split('\n')
-    print(lines)
-
     num1 = [20, 30, 35]
     num2 = [5, 20, 30, 35, 50]
+    print(f'\n****************** Test Case 1 ******************')
+    print(f'num1: {num1}, num2: {num2}')
     ret = main.isSubset(num1, num2)
-    print(f'Your return value is {ret}')  # True
-    assert ret == True
+    print(f'Your return value is {ret}, expected True')  # True
+    assert ret == True, f'Expected True but got {ret}'
 
-
+@pytest.mark.edge
 def test_main_2():
     num1 = [1, 3, 2]
     num2 = [1, 2, 3, 4, 5]
+    print(f'\n****************** Test Case 2 ******************')
+    print(f'num1: {num1}, num2: {num2}')
     ret = main.isSubset(num1, num2)
-    print(f'Your return value is {ret}')  # True
-    assert ret == False
+    print(f'Your return value is {ret}, expected False')  # False
+    assert ret == False, f'Expected False but got {ret}'    
 
-
+@pytest.mark.edge
 def test_main_3():
     num1 = [1, 4, 5]
     num2 = [1, 2, 3, 4, 5]
+    print(f'\n****************** Test Case 3 ******************')
+    print(f'num1: {num1}, num2: {num2}')
     ret = main.isSubset(num1, num2)
-    print(f'Your return value is {ret}')  # True
-    assert ret == False
+    print(f'Your return value is {ret}, expected False')  # False
+    assert ret == False, f'Expected False but got {ret}'
 
-
+@pytest.mark.bonus
 def test_main_4():
     num1 = [1, 3, 2]
     num2 = [4, 1, 3, 2, 5]
+    print(f'\n****************** Test Case 4 ******************')
+    print(f'num1: {num1}, num2: {num2}')
     ret = main.isSubset(num1, num2)
-    print(f'Your return value is {ret}')  # True
-    assert ret == True
+    print(f'Your return value is {ret}, expected True')  # True
+    assert ret == True, f'Expected True but got {ret}'
